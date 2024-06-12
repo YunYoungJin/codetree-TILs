9bafa0b07e40c85b07a829510b3ddd6fba5161d4
Youngjin Yun <30815shine@naver.com>
2024-06-12 14:55:06 +0900

    Title: 중앙값 구하기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240612/중앙값 구하기/find-the-median.py
@@ -0,0 +1,16 @@
+a, b, c = map(int, input().split())
+
+if a > b:
+    if a < c :
+        print(a)
+    elif b > c:
+        print(b)
+    else:
+        print(c)
+else:
+    if b < c:
+        print(b)
+    elif a < c:
+        print(c)
+    else:
+        print(a)
