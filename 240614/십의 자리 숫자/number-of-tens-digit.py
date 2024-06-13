be10adea4806c49714912eacb1d93221ab653251
Youngjin Yun <30815shine@naver.com>
2024-06-14 00:48:19 +0900

    Title: 십의 자리 숫자, Time: 155ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240614/십의 자리 숫자/number-of-tens-digit.py
@@ -0,0 +1,11 @@
+count_arr = [0] * 10
+
+arr = list(map(int, input().split()))
+for elem in arr:
+    if elem == 0:
+        break
+    else:
+        count_arr[elem // 10] += 1
+
+for i in range(1, 10):
+    print(f"{i} - {count_arr[i]}")
