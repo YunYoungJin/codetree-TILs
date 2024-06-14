8318f552ca57d4b7cf1ebfca9a097ad0af355a99
Youngjin Yun <30815shine@naver.com>
2024-06-14 15:50:50 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240614/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = list(map(int, input().split()))
+res = [0] * 1001
+satisfied = False
+
+for num in arr:
+    res[num] += 1
+
+for i in range(1000, 0, -1):
+    if res[i] == 1:
+        print(arr[i - 1])
+        satisfied = True
+        break
+if not satisfied:
+    print(-1)
