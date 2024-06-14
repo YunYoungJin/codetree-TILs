bdeedd056ca15dcc22c824558cd83b84002974e2
Youngjin Yun <30815shine@naver.com>
2024-06-14 15:10:32 +0900

    Title: 연속부분수열일까, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240614/연속부분수열일까/contiguous-array-or-not.py
+++ 240614/연속부분수열일까/contiguous-array-or-not.py
@@ -2,12 +2,14 @@ n1, n2 = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-if B[0] in A:
-    start = A.index(B[0])
-    end = start + len(B)
-    if B == A[start:end]:
-        print("Yes")
-    else:
-        print("No")
+satisfied = False
+for idx, value in enumerate(A):
+    if value == B[0]:
+        start = idx
+        end = start + n2
+        if end <= n1 and B == A[start:end]:
+            satisfied = True                
+if satisfied:
+    print("Yes")
 else:
     print("No")
