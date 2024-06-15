a3eb67c213d15d44f6638a3cf46af9cf40dec1d9
Youngjin Yun <30815shine@naver.com>
2024-06-15 20:46:01 +0900

    Title: 왔다 갔던 구역 2, Time: 112ms, Memory: 25MB, Status: Passed - CodeTree

--- 240615/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240615/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -7,13 +7,13 @@ for _ in range(n):
     x, LR = input().split()
     x = int(x)
     if LR == 'L':
-        for i in range(pos, pos - x -1, -1):
+        for i in range(pos - 1, pos - x -1, -1):
             line[i] += 1
         pos = pos - x
     else:
-        for i in range(pos, pos + x + 1):
+        for i in range(pos, pos + x):
             line[i] += 1
-        poas = pos + x
+        pos = pos + x
 cnt = 0
 for i in range(len(line)):
     if line[i] >= 2:
