7f85b1344debbc89afc6bfe720091bb9ba2319c5
Youngjin Yun <30815shine@naver.com>
2024-06-15 22:00:30 +0900

    Title: 흰검 칠하기, Time: 167ms, Memory: 31MB, Status: Passed - CodeTree

--- 240615/흰검 칠하기/painting-white-black.py
+++ 240615/흰검 칠하기/painting-white-black.py
@@ -1,27 +1,25 @@
 n = int(input())
 line = [''] * 200001
-checked = [0] * 200001
-offset = 100000
-pos = offset
+white = [0] * 200001
+black = [0] * 200001
+pos = 100000
 
 for _ in range(n):
     x, LR = input().split()
     x = int(x)
     if LR == 'L':
-        for i in range(pos, pos - x, -1):
-            if line[i] != 'G':
-                line[i] = 'W'
-            checked[i] += 1
-            if checked[i] == 4:
-                line[i] = 'G'
+        for i in range(pos-x+1, pos+1):
+            line[i] = 'W'
+            white[i] += 1
         pos = pos - x + 1
     else:
         for i in range(pos, pos + x):
-            if line[i] != 'G':
-                line[i] = 'B'
-            checked[i] += 1
-            if checked[i] == 4:
-                line[i] = 'G'
+            line[i] = 'B'
+            black[i] += 1
         pos = pos + x - 1
+
+for i in range(len(line)):
+    if white[i] >= 2 and black[i] >= 2:
+        line[i] = 'G'
 
 print(line.count('W'), line.count('B'), line.count('G'))
