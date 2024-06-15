2860756cb286d5cf5169a76f74e00d6c03a9686e
Youngjin Yun <30815shine@naver.com>
2024-06-15 17:51:43 +0900

    Title: 함수를 이용한 온전수 판별, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240615/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -0,0 +1,16 @@
+def is_magic_number(n):
+    if n % 2 == 0:
+        return False
+    elif n % 10 == 5:
+        return False
+    elif n % 3 == 0 and n % 9 != 0:
+        return False
+    else:
+        return True
+
+a, b = map(int, input().split())
+cnt = 0
+for i in range(a, b + 1):
+    if is_magic_number(i):
+        cnt += 1
+print(cnt)
