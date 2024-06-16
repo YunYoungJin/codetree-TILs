423c364d5ec2e8a0c8440cf3ef1990b5861fcc06
Youngjin Yun <30815shine@naver.com>
2024-06-16 23:01:23 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 202ms, Memory: 24MB, Status: Passed - CodeTree

--- 240616/괄호 문자열의 적합성 판단/parentheses-string.py
+++ 240616/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -1,15 +1,20 @@
 string = input()
-stack = []
-for item in string:
-    if item == '(':
-        stack.append(item)
-    else:
-        if len(stack) == 0:
-            print("No")
-            break
-        stack.pop()
+def matched(string):
+    stack = []
+    for item in string:
+        if item == '(':
+            stack.append(item)
+        else:
+            if len(stack) == 0:
+                return False
+            stack.pop()
 
-if len(stack) != 0:
-    print("No")
-else:
+    if len(stack) != 0:
+        return False
+    
+    return True
+
+if matched(string):
     print("Yes")
+else:
+    print("No")
