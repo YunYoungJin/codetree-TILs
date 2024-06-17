82ea1dc0437242c26c46847d016fc62a39eb93b3
Youngjin Yun <30815shine@naver.com>
2024-06-17 22:48:42 +0900

    Title: 선두를 지켜라 3, Time: 228ms, Memory: 45MB, Status: Passed - CodeTree

---
+++ 240617/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,28 @@
+n, m = map(int, input().split())
+
+A = [0]
+B = [0]
+
+first = None
+cnt = 0
+
+for _ in range(n):
+    v, t = map(int, input().split())
+    for i in range(1, t + 1):
+        A.append(A[-1] + v)
+for _ in range(m):
+    v, t = map(int, input().split())
+    for i in range(1, t + 1):
+        B.append(B[-1] + v)
+
+for i in range(1, len(A)):
+    if A[i] > B[i] and first != 'A':
+        cnt += 1
+        first = 'A'
+    elif A[i] < B[i] and first != 'B':
+        cnt += 1
+        first = 'B'
+    elif A[i] == B[i] and first != 'both':
+        cnt += 1
+        first = 'both'
+print(cnt)
