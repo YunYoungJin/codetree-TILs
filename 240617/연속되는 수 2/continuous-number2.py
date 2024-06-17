4e227ba42f824d2c118cca350ab95f41e5228ea9
Youngjin Yun <30815shine@naver.com>
2024-06-17 20:06:55 +0900

    Title: 연속되는 수 2, Time: 161ms, Memory: 25MB, Status: Passed - CodeTree

--- 240617/연속되는 수 2/continuous-number2.py
+++ 240617/연속되는 수 2/continuous-number2.py
@@ -1,13 +1,19 @@
 n = int(input())
 arr= [int(input()) for _ in range(n)]
 
+new = False
 max_cnt = 0
 cnt = 1
 
-for i in range(n):
-    if i == 0 or arr[i] != arr[i - 1]:
+for i in range(1, n):
+    if arr[i] != arr[i - 1]:
         max_cnt = max(max_cnt, cnt)
         cnt = 1
+        new = True
     else:
         cnt += 1
-print(max_cnt)
+
+if not new:
+    print(len(arr))
+else:
+    print(max_cnt)
