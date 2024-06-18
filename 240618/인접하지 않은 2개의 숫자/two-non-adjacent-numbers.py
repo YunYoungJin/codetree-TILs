caa70e956ec184a61adb9921967e577ecd042eec
Youngjin Yun <30815shine@naver.com>
2024-06-18 16:55:51 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240618/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.py
@@ -0,0 +1,8 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+max_val = 0
+for i in range(n - 1):
+    for j in range(i + 2, n):
+        max_val = max(max_val, arr[i] + arr[j])
+print(max_val)
