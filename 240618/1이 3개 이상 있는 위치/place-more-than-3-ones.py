5d42beb1117a41f64185ef4ee6b20199feb7da6c
Youngjin Yun <30815shine@naver.com>
2024-06-18 14:42:15 +0900

    Title: 1이 3개 이상 있는 위치, Time: 152ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240618/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,19 @@
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+
+ans = 0
+dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+for i in range(n):
+    for j in range(n):
+        cnt = 0
+        for dx, dy in zip(dxs, dys):
+            nx, ny = i + dx, j + dy
+            if in_range(nx, ny) and board[nx][ny] == 1:
+                cnt += 1
+        if cnt >= 3:
+            ans += 1
+print(ans)
