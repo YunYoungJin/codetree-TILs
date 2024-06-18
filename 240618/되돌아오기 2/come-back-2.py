d6adc42a3476b40ee0e0cc7e9c7ecffd1095d9b1
Youngjin Yun <30815shine@naver.com>
2024-06-18 15:29:41 +0900

    Title: 되돌아오기 2, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240618/되돌아오기 2/come-back-2.py
@@ -0,0 +1,18 @@
+dx, dy = [1, 0, -1, 0], [0, -1, 0, 1] # E, S, W, N
+dir_num = 3 
+x, y = 0, 0
+
+s = input()
+t = 0
+
+for item in s:
+    t += 1
+    if item == 'L':
+        dir_num = (dir_num + 3) % 4
+    elif item == 'R':
+        dir_num = (dir_num + 1) % 4
+    else:
+        x, y = x + dx[dir_num], y + dy[dir_num]
+        if (x, y) == (0, 0):
+            break
+print(t)
