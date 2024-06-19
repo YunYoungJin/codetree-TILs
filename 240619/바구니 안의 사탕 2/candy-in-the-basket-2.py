d7e6faca4a29ffc498a803225691aad920595fff
Youngjin Yun <30815shine@naver.com>
2024-06-19 17:45:36 +0900

    Title: 바구니 안의 사탕 2, Time: 111ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240619/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+
+line = [0] * 101
+for _ in range(n):
+    candy, pos = map(int, input().split())
+    line[pos] = candy
+
+max_cnt = 0
+for i in range(k, 101 - k):
+    cnt = 0
+    for j in range(i - k, i + k +1):
+        cnt += line[j]
+    max_cnt = max(max_cnt, cnt)
+print(max_cnt)
