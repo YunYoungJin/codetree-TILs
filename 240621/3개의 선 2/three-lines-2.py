a0e6d469a9a2757028baee505decd571468afa55
Youngjin Yun <30815shine@naver.com>
2024-06-21 16:45:29 +0900

    Title: 3개의 선 2, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

--- 240621/3개의 선 2/three-lines-2.py
+++ 240621/3개의 선 2/three-lines-2.py
@@ -7,23 +7,23 @@ points = [
     for _ in range(n)
 ]
 
-is_possible = False
-
 # 가로, 세로 0, 3
 for i in range(11):
     for j in range(11):
         for k in range(11):
             # 각 직선 조합마다 가능성 체크
-            passed = False
+            is_possible = True
             # 모든 점에 대해서
             for x, y in points:
+                passed = False
                 if x == i or x == j or x == k:
                     passed = True
                 # 한 점이라도 불가능하면 다음 조합
                 if not passed:
+                    is_possible = False
                     break
             # 가능하면
-            if passed:
+            if is_possible:
                 print(1)
                 sys.exit(0)
 
@@ -32,16 +32,18 @@ for i in range(11):
     for j in range(11):
         for k in range(11):
             # 각 직선 조합마다 가능성 체크
-            passed = False
+            is_possible = True
             # 모든 점에 대해서
             for x, y in points:
+                passed = False
                 if x == i or x == j or y == k:
                     passed = True
                 # 한 점이라도 불가능하면 다음 조합
                 if not passed:
+                    is_possible = False
                     break
             # 가능하면
-            if passed:
+            if is_possible:
                 print(1)
                 sys.exit(0)
 
@@ -50,13 +52,15 @@ for i in range(11):
 for i in range(11):
     for j in range(11):
         for k in range(11):
-            passed = False
+            is_possible = True
             for x, y in points:
+                passed = False
                 if x == i or y == j or y == k:
                     passed = True
                 if not passed:
+                    is_possible = False
                     break
-            if passed:
+            if is_possible:
                 print(1)
                 sys.exit(0)
 
@@ -65,16 +69,18 @@ for i in range(11):
     for j in range(11):
         for k in range(11):
             # 각 직선 조합마다 가능성 체크
-            passed = False
+            is_possible = True
             # 모든 점에 대해서
             for x, y in points:
+                passed = False
                 if y == i or y == j or y == k:
                     passed = True
                 # 한 점이라도 불가능하면 다음 조합
                 if not passed:
+                    is_possible = False
                     break
             # 가능하면
-            if passed:
+            if is_possible:
                 print(1)
                 sys.exit(0)
 
