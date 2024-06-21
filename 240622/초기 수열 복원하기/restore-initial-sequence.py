2cb9ff122faabc066189372e2ace50a470db05d8
Youngjin Yun <30815shine@naver.com>
2024-06-22 00:26:19 +0900

    Title: 초기 수열 복원하기, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240622/초기 수열 복원하기/restore-initial-sequence.py
+++ 240622/초기 수열 복원하기/restore-initial-sequence.py
@@ -2,8 +2,8 @@ n = int(input())
 sums = list(map(int, input().split()))
 ans = []
 
-# 수열의 첫항으로 가능한 수는 1 ~ (sums[0] - 1)
-for i in range(1, sums[0]):
+# 수열의 첫항이 될 수
+for i in range(1, n + 1):
     # 수열 생성이 가능한지
     is_possible = True
     # 임시 수열
@@ -12,13 +12,13 @@ for i in range(1, sums[0]):
     exist=[False for _ in range(n + 1)]
     exist[i] = True
 
-    # i로 시작하는 수열을 sum을 기반으로 마지막항까지 확인
+    # i로 시작하는 수열을 sums를 기반으로 마지막항까지 확인
     for j in range(n - 1):
         # j + 1 번째 항에 나올 수
         next_num = sums[j] - tmp[j]
         
-        # 이미 등장한 수라면 수열 생성 불가
-        if exist[next_num] == True:
+        # 등장 할 수 없는 수거나 이미 등장한 수라면 수열 생성 불가
+        if (next_num < 1 or next_num > n) or exist[next_num] == True:
             is_possible = False
             break
         # 그 수가 등장한 적이 없다면 갱신
