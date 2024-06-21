6abd5dead4b7675d9f2ce244b6bbc82320216cb0
Youngjin Yun <30815shine@naver.com>
2024-06-21 18:25:37 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240621/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 240621/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -3,11 +3,19 @@ s = input()
 
 length = 101
 
-for i in range(1, n // 2 + 2):
+# 부분 문자열 길이가 i일 때
+for i in range(1, n + 1):
     once = True
+    # 부분 문자열의 시작 index가 j
     for j in range(n):
-        if j + i < n:
-            if s.count(s[j:j + i]) >= 2:
+        # 부분 문자열이 생성 가능하면
+        if j + i <= n:
+            cnt = 0
+            sub = s[j:j + i]
+            for k in range(len(s) - i + 1):
+                if s[k:k + i] == sub:
+                    cnt += 1
+            if cnt != 1:
                 once = False
                 break
     if once:
