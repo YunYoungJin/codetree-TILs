dc9bbdd18432d91f63705c1fa172854b7cb3a107
Youngjin Yun <30815shine@naver.com>
2024-06-23 01:40:38 +0900

    Title: 코딩톡, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240623/코딩톡/coding-talk.py
+++ 240623/코딩톡/coding-talk.py
@@ -12,22 +12,13 @@ infos = [
 
 # p번째 메시지를 보낸 사람, 안 읽은 사람 수
 pc, pu = infos[p - 1]
-
-# p번째 메시지를 포함한 이후 메시지를 보낸 사람들은
-# 메세지를 확실히 읽었다
 reader_list = []
 if int(pu) == 0:
     reader_list = talkers
-else:
-    reader_list.append(pc)
 
-if p >= 2 and pu == infos[p - 2][1]:
-    if infos[p - 2][0] not in reader_list:
-        reader_list.append(infos[p - 2][0])
-
-for idx in range(p, m):
-    c, u = infos[idx]
-    if c not in reader_list:
-        reader_list.append(c)
+for c, u in infos:
+    if int(u) >= int(pu):
+        if c not in reader_list:
+            reader_list.append(c)
 
 print(*(set(talkers) - set(reader_list)))
