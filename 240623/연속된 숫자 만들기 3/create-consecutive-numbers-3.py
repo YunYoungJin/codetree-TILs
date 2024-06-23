95dd052e2aea972a7a93a860b7e11aa50a20ad6b
Youngjin Yun <30815shine@naver.com>
2024-06-23 20:41:03 +0900

    Title: 연속된 숫자 만들기 3, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240623/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 240623/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -1,15 +1,13 @@
 x = list(map(int, input().split()))
 cnt = 0
 
-while not (x[2] - x[1] == 1 and x[1] - x[0] == 1):
-    d1, d2 = x[1] - x[0], x[2] - x[1]
-    
-    if d1 > d2:
-        x[2] = x[1] - 1
-    else:
-        x[0] = x[1] + 1
-    
-    x.sort()
-    cnt += 1
+d1, d2 = x[1] - x[0], x[2] - x[1]
+
+if d1 == 1 and d2 == 1:
+    cnt = 0
+elif d1 > d2:
+    cnt = d1 - 1
+else:
+    cnt = d2 - 1
 
 print(cnt)
