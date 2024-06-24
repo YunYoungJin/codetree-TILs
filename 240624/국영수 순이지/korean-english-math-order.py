91eb9d9e6612c711f9b1f202a006d4552bfa3ed4
Youngjin Yun <30815shine@naver.com>
2024-06-24 14:32:18 +0900

    Title: 국영수 순이지, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240624/국영수 순이지/korean-english-math-order.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+infos = [
+    tuple(input().split())
+    for _ in range(n)
+]
+
+infos.sort(key = lambda x : (-int(x[1]), -int(x[2]), -int(x[3])))
+
+for name, kor, eng, math in infos:
+    print(f"{name} {kor} {eng} {math}")
