b1343c9f90bcf414f5e1f824683f7d07f3b825e4
Youngjin Yun <30815shine@naver.com>
2024-06-25 15:54:28 +0900

    Title: 트로미노, Time: 165ms, Memory: 26MB, Status: Passed - CodeTree

--- 240625/트로미노/tromino.py
+++ 240625/트로미노/tromino.py
@@ -8,16 +8,21 @@ max_sum = 0
 
 # L자 블럭
 for i in range(n - 1):
-    for j in range(n - 1):
+    for j in range(m - 1):
         tmp = grid[i][j] + grid[i][j + 1] + grid[i + 1][j] + grid[i + 1][j + 1]
         tmp -= min(grid[i][j], grid[i][j + 1], grid[i + 1][j], grid[i + 1][j + 1])
         max_sum = max(max_sum, tmp)
 
-# I자 블럭
+# I자 블럭 가로
 for i in range(n):
-    for j in range(n - 2):
-        tmp1 = grid[i][j] + grid[i][j + 1] + grid[i][j + 2]
-        tmp2 = grid[j][i] + grid[j + 1][i] + grid[j + 2][i]
-        max_sum = max(max_sum, tmp1, tmp2)
+    for j in range(m - 2):
+        tmp = grid[i][j] + grid[i][j + 1] + grid[i][j + 2]
+        max_sum = max(max_sum, tmp)
+
+# I자 블럭 세로
+for j in range(m):
+    for i in range(n - 2):
+        tmp = grid[i][j] + grid[i + 1][j] + grid[i + 2][j]
+        max_sum = max(max_sum, tmp)
 
 print(max_sum)
