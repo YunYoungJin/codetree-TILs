dd7041b05bb334d15dd8c66cf4081b8e47e91ed7
Youngjin Yun <30815shine@naver.com>
2024-06-25 16:21:54 +0900

    Title: 컨베이어 벨트, Time: 149ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240625/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+n, t = map(int, input().split())
+belts = deque(list(map(int, input().split())) + list(map(int, input().split())))
+
+for _ in range(t):
+    belts.rotate(1)
+
+for i in range(len(belts)):
+    print(belts[i], end=' ')
+    if i % 3 == 2:
+        print()
