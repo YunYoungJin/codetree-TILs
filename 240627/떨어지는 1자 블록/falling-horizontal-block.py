1603836ee10391931550236f9bae404b382ee614
Youngjin Yun <30815shine@naver.com>
2024-06-27 14:39:09 +0900

    Title: 떨어지는 1자 블록, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240627/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240627/떨어지는 1자 블록/falling-horizontal-block.py
@@ -7,15 +7,20 @@ grid = [
 
 go_down = True
 
-for row in range(n - 1):
-    for col in range(k, k + m):
-        if grid[row + 1][col - 1] == 1:
-            go_down = False
-            break
-    if not go_down:
+for row in range(n):
+    if row == n - 1:
         for col in range(k, k + m):
             grid[row][col - 1] = 1
-        break
+    else:
+        for col in range(k, k + m):
+            if grid[row + 1][col - 1] == 1:
+                go_down = False
+                break
+    
+        if not go_down:
+            for col in range(k, k + m):
+                grid[row][col - 1] = 1
+            break
 
 for row in grid:
     print(*row)
