2306dd7a1c6f520a7b3ac5e96b705a3f4bf73ef5
Youngjin Yun <30815shine@naver.com>
2024-06-28 18:23:54 +0900

    Title: 최대 감소 부분 수열, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240628/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = [10001] + list(map(int, input().split()))
+
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    for j in range(i):
+        if arr[i] < arr[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
