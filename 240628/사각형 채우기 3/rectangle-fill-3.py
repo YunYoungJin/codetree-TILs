7115369cf180dbad70b31af7c4d29913bdb6541d
Youngjin Yun <30815shine@naver.com>
2024-06-28 16:47:41 +0900

    Title: 사각형 채우기 3, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240628/사각형 채우기 3/rectangle-fill-3.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+dp = [0] * (n + 2)
+
+dp[0] = 1
+dp[1] = 2
+dp[2] = 7
+
+for i in range(3, n + 1):
+    dp[i] = 3 * dp[i - 1] + dp[i - 2] - dp[i - 3]
+
+print(dp[n] % 1000000007)
