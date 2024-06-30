6cecaf00dfc333b61b603fc5ff6a231b2d4944af
Youngjin Yun <30815shine@naver.com>
2024-06-30 21:54:27 +0900

    Title: 부분 수열의 합이 m, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240630/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
@@ -0,0 +1,19 @@
+import sys
+INT_MAX = sys.maxsize
+
+n, m = map(int, input().split())
+arr = [0] + list(map(int, input().split()))
+
+dp = [0] + [INT_MAX] * m
+
+for i in range(1, n + 1):
+    for j in range(m, -1, -1):
+        if j >= arr[i]:
+            if dp[j - arr[i]] == INT_MAX:
+                continue
+            dp[j] = min(dp[j], dp[j - arr[i]] + 1)
+
+if dp[m] ==  INT_MAX:
+    print(-1)
+else:
+    print(dp[m])
