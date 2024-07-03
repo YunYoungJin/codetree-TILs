d57599d5ed9ed396d9e35af953ad9ae9196f0822
Youngjin Yun <30815shine@naver.com>
2024-07-03 15:37:08 +0900

    Title: 안전 지대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240703/안전 지대/comfort-zone.py
+++ 240703/안전 지대/comfort-zone.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(10 ** 7)
+
 n, m = map(int, input().split())
 grid = [
     list(map(int, input().split()))
@@ -8,17 +11,20 @@ grid = [
 dxs = [-1, 1, 0, 0]
 dys = [0, 0, -1, 1]
 
+
 def in_range(x, y):
     return (0 <= x and x < n) and (0 <= y and y < m)
+
 
 def can_go(nx, ny, k):
     if not in_range(nx, ny):
         return False
-    
+
     if visited[nx][ny] or (grid[nx][ny] <= k):
         return False
 
     return True
+
 
 def dfs(curr_x, curr_y, k):
     for dx, dy in zip(dxs, dys):
@@ -27,6 +33,7 @@ def dfs(curr_x, curr_y, k):
         if can_go(nx, ny, k):
             visited[nx][ny] = 1
             dfs(nx, ny, k)
+
 
 safe_zones = []
 
