3963a8cf200e581becb050ff96a512b3ac9c7b3f
Youngjin Yun <30815shine@naver.com>
2024-07-04 14:07:52 +0900

    Title: 숫자 합치기, Time: 318ms, Memory: 32MB, Status: Passed - CodeTree

--- 240704/숫자 합치기/merge-numbers.py
+++ 240704/숫자 합치기/merge-numbers.py
@@ -1,12 +1,19 @@
+import heapq
+
 n = int(input())
 arr = list(map(int, input().split()))
+heap = []
+
+for num in arr:
+    heapq.heappush(heap, num)
 
 ans = 0
 
-while len(arr) > 1:
-    arr.sort(reverse=True)
-    new_num = arr.pop() + arr.pop()
+while len(heap) != 1:
+    first = heapq.heappop(heap)
+    second = heapq.heappop(heap)
+    new_num = first + second
     ans += new_num
-    arr.append(new_num)
+    heapq.heappush(heap, new_num)
 
 print(ans)
