1d5bc543db19403a830fd0693dd94c7eaba23283
Youngjin Yun <30815shine@naver.com>
2024-07-04 15:32:18 +0900

    Title: 정수 찾기, Time: 187ms, Memory: 37MB, Status: Passed - CodeTree

---
+++ 240704/정수 찾기/find-an-integer.py
@@ -0,0 +1,10 @@
+n = int(input())
+s1 = set(list(map(int, input().split())))
+m = int(input())
+s2 = list(map(int, input().split()))
+
+for item in s2:
+    if item in s1:
+        print(1)
+    else:
+        print(0)
