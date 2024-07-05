d23dda7258163cae3ad5085135c38374b0cca319
Youngjin Yun <30815shine@naver.com>
2024-07-05 17:49:29 +0900

    Title: 자리 바꾸기 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240705/자리 바꾸기 2/changing-seats-2.py
@@ -0,0 +1,20 @@
+n, k = map(int, input().split())
+infos = [
+    tuple(map(int, input().split()))
+    for _ in range(k)
+]
+
+seat = [i for i in range(n + 1)]
+seat_check= [
+    set([i])
+    for i in range(n + 1)
+]
+
+for _ in range(3):
+    for a, b in infos:
+        seat[a], seat[b] = seat[b], seat[a]
+        seat_check[seat[a]].add(b)
+        seat_check[seat[b]].add(a)
+
+for i in range(1, n + 1):
+    print(len(seat_check[i]))
