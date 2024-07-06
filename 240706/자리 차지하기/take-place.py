2cd24b959a3bac625c7caa4144cb5b12f56835b6
Youngjin Yun <30815shine@naver.com>
2024-07-06 12:04:20 +0900

    Title: 자리 차지하기, Time: 274ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240706/자리 차지하기/take-place.py
+++ 240706/자리 차지하기/take-place.py
@@ -3,19 +3,19 @@ from sortedcontainers import SortedSet
 
 n, m = map(int, input().split())
 arr = list(map(int, sys.stdin.readline().split()))
-s = SortedSet()
+s = SortedSet(range(1, m + 1))
 
-for a in arr:
-    can_sit = False
-    for i in range(a, 0, -1):
-        if i in s:
-            continue
-        else:
-            can_sit = True
-            s.add(i)
-            break
-    
-    if not can_sit:
+ans = 0
+
+for num in arr:
+    idx = s.bisect_right(num)
+
+    if idx != 0:
+        idx -= 1
+        s.remove(s[idx])
+
+        ans += 1
+    else:
         break
 
 print(len(s))
