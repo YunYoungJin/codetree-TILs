84171dd739deee5b64622b3e04fd2069fb4607c3
Youngjin Yun <30815shine@naver.com>
2024-07-07 15:57:06 +0900

    Title: 기울어진 직사각형의 회전, Time: 166ms, Memory: 25MB, Status: Passed - CodeTree

--- 240707/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 240707/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -7,8 +7,8 @@ grid = [
 r, c, m1, m2, m3, m4, clockwise = map(int, input().split())
 r, c = r - 1, c - 1
 
-# 반시계 순회(0, 1 , 2 3)
-# 시계 순회(1, 0, 3, 2)
+# 반시계 순회(0, 1, 2, 3)
+#   시계 순회(1, 0, 3, 2)
 dxs = [-1, -1, 1, 1]
 dys = [1, -1, -1, 1]
 
@@ -34,7 +34,7 @@ def rotate_cw(r, c, p, q):
     
     for _ in range(q - 1):
         nx, ny = x + dxs[3], y + dys[3]
-        grid[x][y] - grid[nx][ny]
+        grid[x][y] = grid[nx][ny]
         x, y = nx, ny
     
     grid[x][y] = temp
