9b2a3acb728c30c0e3ab040cdc795924dc60069e
Youngjin Yun <30815shine@naver.com>
2024-07-09 18:42:50 +0900

    Title: 정수 n개의 합 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240709/정수 n개의 합 2/sum-of-n-integers-2.py
@@ -0,0 +1,14 @@
+import sys
+
+n, k = map(int, input().split())
+arr = [0] + list(map(int, input().split()))
+
+prefix_sum = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    prefix_sum[i] = prefix_sum[i - 1] + arr[i]
+
+ans = -sys.maxsize
+
+for i in range(1, n - k + 2):
+    ans = max(prefix_sum[i + k - 1] - prefix_sum[i])
