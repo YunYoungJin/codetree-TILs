3898dfcf23f265a3936898d535122c219a1d6b2c
Youngjin Yun <30815shine@naver.com>
2024-07-10 21:43:49 +0900

    Title: 부분 수열의 합이 k, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240710/부분 수열의 합이 k/the-sum-of-the-subsequences-is-k.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+arr = [0] + list(map(int, input().split()))
+prefix_sum = [0] * (n + 1)
+ans = 0
+
+for i in range(1, n + 1):
+    prefix_sum[i] = prefix_sum[i - 1] + arr[i]
+
+for i in range(1, n + 1):
+    for j in range(i + 1, n + 1):
+        tmp_sum = prefix_sum[j] - prefix_sum[i - 1]
+        if tmp_sum == k:
+            ans += 1
+
+print(ans)
