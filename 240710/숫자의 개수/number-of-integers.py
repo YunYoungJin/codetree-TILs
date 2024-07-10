cc45591ea1b215ffcaf343adfa857e53f24eef30
Youngjin Yun <30815shine@naver.com>
2024-07-10 14:19:47 +0900

    Title: 숫자의 개수, Time: 538ms, Memory: 33MB, Status: Passed - CodeTree

---
+++ 240710/숫자의 개수/number-of-integers.py
@@ -0,0 +1,42 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+def find_lower_bound(x):
+    left = 0
+    right = n - 1
+    min_idx = n
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] >= x:
+            min_idx = min(min_idx, mid)
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return min_idx
+
+
+def find_upper_bound(x):
+    left = 0
+    right = n - 1
+    min_idx = n
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] > x:
+            min_idx = min(min_idx, mid)
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return min_idx
+
+for _ in range(m):
+    pivot = int(input())
+
+    lb = find_lower_bound(pivot)
+    ub = find_upper_bound(pivot)
+    print(ub - lb)
