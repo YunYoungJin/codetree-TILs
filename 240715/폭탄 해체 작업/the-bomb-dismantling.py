a047a41ba7b31118cc640a6b1023d16a6bece4b9
Youngjin Yun <30815shine@naver.com>
2024-07-15 21:40:12 +0900

    Title: 폭탄 해체 작업, Time: 170ms, Memory: 26MB, Status: Passed - CodeTree

--- 240715/폭탄 해체 작업/the-bomb-dismantling.py
+++ 240715/폭탄 해체 작업/the-bomb-dismantling.py
@@ -4,14 +4,18 @@ bombs = [
     for _ in range(n)
 ]
 
-bombs.sort(key=lambda x: (x[1], -x[0]))
+bombs.sort(key=lambda x: -x[0])
 
-last_e = 0
-ans = 0
+max_time = max(bomb[1] for bomb in bombs)
+used = [False] * (max_time + 1)  # 특정 시간을 사용했는지 확인하는 리스트
+total_score = 0
 
-for score, t in bombs:
-    if last_e < t:
-        ans += score
-        last_e = t
+for score, time_limit in bombs:
+    # 가능한 가장 늦은 시간부터 시작해서 시간이 비어 있는지 확인
+    for t in range(time_limit, 0, -1):
+        if not used[t]:
+            used[t] = True
+            total_score += score
+            break
 
-print(ans)
+print(total_score)
