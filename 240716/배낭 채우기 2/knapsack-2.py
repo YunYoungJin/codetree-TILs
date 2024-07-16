a5730e7ce366a1ceaa4d6c4080ad99f3aeefba0e
Youngjin Yun <30815shine@naver.com>
2024-07-16 17:22:28 +0900

    Title: 배낭 채우기 2, Time: 116ms, Memory: 28MB, Status: Passed - CodeTree

--- 240716/배낭 채우기 2/knapsack-2.py
+++ 240716/배낭 채우기 2/knapsack-2.py
@@ -4,16 +4,12 @@ infos = [
     for _ in range(n)
 ]
 
+# 무게의 합이 i 이하, 얻을 수 있는 최대 가치
 dp = [0] * (m + 1)
 
 for i in range(1, m + 1):
-
     for w, v in infos:
-        if i < w:
-            continue
-        else:
-            dp[i] = max(dp[i], v)
-            for j in range(i - w, i):
-                dp[i] = max(dp[i - w] + v, dp[j], dp[i])
+        if i >= w:
+            dp[i] = max(dp[i - w] + v, dp[i])
 
 print(max(dp))
