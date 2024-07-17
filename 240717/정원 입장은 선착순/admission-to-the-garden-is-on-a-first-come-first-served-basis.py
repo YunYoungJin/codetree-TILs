5eda3b3d32252ebad62ceb98a6c02cf5e3581629
Youngjin Yun <30815shine@naver.com>
2024-07-17 22:17:39 +0900

    Title: 정원 입장은 선착순, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240717/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
@@ -0,0 +1,45 @@
+import heapq
+
+n = int(input())
+infos = []
+
+for i in range(n):
+    a, t = map(int, input().split())
+    infos.append((a, t, i))
+
+infos.sort(key=lambda x: (x[0], x[2]))
+
+waiting_time = [0] * n
+waiting_list = []
+
+last_exit_time = 0
+ans = 0
+
+for a, t, i in infos:
+    # 이전 사람의 퇴장 시간보다 일찍 도착했다면 대기열에 추가
+    if a < last_exit_time:
+        heapq.heappush(waiting_list, (i, a, t))
+
+    else:
+        # 나중에 도착했고 대기열에 사람이 없다면
+        if not waiting_list:
+            last_exit_time = a + t
+
+        # 나중에 도착했지만 대기열에 사람이 있다면
+        else:
+            while waiting_list:
+                next_i, next_a, next_t = waiting_list[0]
+                if last_exit_time + next_t <= a:
+                    heapq.heappop(waiting_list)
+                    waiting_time[next_i] = last_exit_time - next_a
+                    last_exit_time += next_t
+                else:
+                    heapq.heappush(waiting_list, (i, a, t))
+                    break
+
+while waiting_list:
+    next_i, next_a, next_t = heapq.heappop(waiting_list)
+    waiting_time[next_i] = last_exit_time - next_a
+    last_exit_time += next_t
+
+print(max(waiting_time))
