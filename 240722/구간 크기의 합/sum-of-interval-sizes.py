00fd9f4e0785dc7b17084680d0d5afc79210ab9f
Youngjin Yun <30815shine@naver.com>
2024-07-22 17:08:35 +0900

    Title: 구간 크기의 합, Time: 967ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 240722/구간 크기의 합/sum-of-interval-sizes.py
@@ -0,0 +1,26 @@
+n = int(input())
+segments = []
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    segments.append((a, 1))
+    segments.append((b, -1))
+
+segments.sort()
+
+ans = 0
+tmp_sum = 0
+start = None
+
+for x, v in segments:
+    tmp_sum += v
+
+    if tmp_sum > 0:
+        if start is None:
+            start = x
+    
+    if tmp_sum == 0:
+        ans += x - start
+        start = None
+
+print(ans)
