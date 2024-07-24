9a55aa39a0972db5f046f965743f123fba64f6eb
Youngjin Yun <30815shine@naver.com>
2024-07-24 16:57:43 +0900

    Title: 수영장 효율적으로 활용하기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240724/수영장 효율적으로 활용하기/use-the-swimming-pool-efficiently.py
@@ -0,0 +1,31 @@
+n, m = map(int, input().split())
+ts = list(map(int, input().split()))
+
+left = 1
+right = sum(ts)
+ans = 100001
+
+def is_possible(x):
+    cnt = 1
+    time = 0
+
+    for i in range(n):
+        if time + ts[i] > x:
+            time = ts[i]
+            cnt += 1
+        else:
+            time += ts[i]
+    
+    return cnt <= m
+
+
+while left <= right:
+    mid = (left + right) // 2
+
+    if is_possible(mid):
+        ans = min(ans, mid)
+        right = mid - 1
+    else:
+        left = mid + 1
+
+print(ans)
