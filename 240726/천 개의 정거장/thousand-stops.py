247433e97146ba11c9d86c516225afeb16a91e60
Youngjin Yun <30815shine@naver.com>
2024-07-26 14:31:06 +0900

    Title: 천 개의 정거장, Time: 1049ms, Memory: 68MB, Status: Passed - CodeTree

--- 240726/천 개의 정거장/thousand-stops.py
+++ 240726/천 개의 정거장/thousand-stops.py
@@ -14,10 +14,18 @@ def dijkstra(start, end, graph, N):
     while pq:
         current_cost, current_time, current_node = heapq.heappop(pq)
 
+        if current_cost != distances[current_node][0]:
+            continue
+
         if current_node == end:
             return current_cost, current_time
 
-        for next_cost, next_time, neighbor in graph[current_node]:
+        for neighbor in range(1, 1001):
+            next_cost, next_time = graph[current_node][neighbor]
+
+            if next_cost == INT_MAX:
+                continue
+
             new_cost = current_cost + next_cost
             new_time = current_time + next_time
 
@@ -27,18 +35,17 @@ def dijkstra(start, end, graph, N):
 
     return -1, -1
 
-graph = {i: [] for i in range(1, 1001)}
+graph = [[(INT_MAX, INT_MAX) for _ in range(1001)] for _ in range(1001)]
 
 for i in range(1, n + 1):
     cost, stop_cnt = map(int, input().split())
     stops = list(map(int, input().split()))
 
-    for i in range(stop_cnt - 1):
-        for j in range(i, stop_cnt):
-            if i == j:
-                continue
+    for i in range(stop_cnt):
+        for j in range(i + 1, stop_cnt):
             time = j - i
-            graph[stops[i]].append((cost, time, stops[j]))
+            if graph[stops[i]][stops[j]][0] > cost:
+                graph[stops[i]][stops[j]] = (cost, time)
 
 min_cost, min_time = dijkstra(a, b, graph, n)
 
