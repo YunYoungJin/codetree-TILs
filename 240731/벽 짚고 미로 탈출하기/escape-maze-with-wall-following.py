8e5a080559f0d7709f697da4c1f1348d31a8a6cf
Youngjin Yun <30815shine@naver.com>
2024-07-31 17:37:04 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 180ms, Memory: 25MB, Status: Passed - CodeTree

--- 240731/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 240731/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -11,7 +11,6 @@ directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 dir_idx = 0
 
 visited = set()
-visited.add((x, y, dir_idx))
 
 ans = 0
 
@@ -19,6 +18,11 @@ def in_range(x, y):
     return (0 <= x and x < n) and (0 <= y and y < n)
 
 while True:
+    if (x, y, dir_idx) in visited:
+        ans = -1
+        break
+
+    visited.add((x, y, dir_idx))
     dx, dy = directions[dir_idx]
     nx, ny = x + dx, y + dy
 
@@ -27,29 +31,23 @@ while True:
         ans += 1
         break
 
-    # 탈출하지 않았는데, 이미 방문했던 곳이라면 탈출 불가
-    if (nx, ny, dir_idx) in visited:
-        ans = -1
-        break
-
     # 벽이 있는 경우: 반 시계 방향으로 회전
     if grid[nx][ny] == '#':
         dir_idx = (dir_idx - 1) % 4
     else:
-        x, y = nx, ny
-        visited.add((x, y, dir_idx))
-        ans += 1
-
         # 오른쪽 벽을 짚고 가기 위한 계산
         right_dir_idx = (dir_idx + 1) % 4
         right_x, right_y = nx + directions[right_dir_idx][0], ny + directions[right_dir_idx][1]
 
         # 이동할 수 있고 오른쪽에 벽이 있는 경우: 한 칸 이동
         if in_range(right_x, right_y):
-            if grid[right_x][right_y] != '#':
-                dir_idx = right_dir_idx
-                x, y = right_x, right_y
-                visited.add((x, y, dir_idx))
+            if grid[right_x][right_y] == '#':
+                x, y = nx, ny
                 ans += 1
+            else:
+                dir_idx = right_dir_idx
+                visited.add((nx, ny, dir_idx))
+                x, y = right_x, right_y
+                ans += 2
 
 print(ans)
