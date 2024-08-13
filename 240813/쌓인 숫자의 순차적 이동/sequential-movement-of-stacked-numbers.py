56dc14fde7e2129b913efa0afaa76ae836eac344
Youngjin Yun <30815shine@naver.com>
2024-08-13 20:25:41 +0900

    Title: 쌓인 숫자의 순차적 이동, Time: 157ms, Memory: 25MB, Status: Passed - CodeTree

--- 240813/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.py
+++ 240813/쌓인 숫자의 순차적 이동/sequential-movement-of-stacked-numbers.py
@@ -12,18 +12,17 @@ for i in range(n):
         grid[i][j] = after_move[i][j][0]
         number_pos[grid[i][j]] = (i, j)
 
-
 for num in queries:
     x, y = number_pos[num]
     max_value = 0
     best_pos = (x, y)
 
     for dx, dy in [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < n and 0 <= ny < n:
-                if grid[nx][ny] > max_value:
-                    max_value = grid[nx][ny]
-                    best_pos = (nx, ny)
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < n and 0 <= ny < n:
+            if grid[nx][ny] > max_value:
+                max_value = grid[nx][ny]
+                best_pos = (nx, ny)
 
     if max_value == 0:
         continue
@@ -39,13 +38,14 @@ for num in queries:
 
     if len(after_move[x][y]) == 0:
         grid[x][y] = 0
-    
+    else:
+        grid[x][y] = max(after_move[x][y])
+
     while len(tmp) != 0:
         tmp_num = tmp.pop()
         after_move[nx][ny].appendleft(tmp_num)
         number_pos[tmp_num] = (nx, ny)
     grid[nx][ny] = max(after_move[nx][ny])
-    
 
 for i in range(n):
     for j in range(n):
