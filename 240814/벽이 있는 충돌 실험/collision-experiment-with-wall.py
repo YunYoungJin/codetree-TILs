c8399cc0d37df40f7d2c0b8142108540176a9f0b
Youngjin Yun <30815shine@naver.com>
2024-08-14 21:40:38 +0900

    Title: 벽이 있는 충돌 실험, Time: 336ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240814/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 240814/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -1,51 +1,51 @@
-from collections import deque, defaultdict
+from collections import deque
 
-t = int(input())
 
 def simulate(n, beads):
     direction_map = {'U': (-1, 0), 'D': (1, 0), 'L': (0, -1), 'R': (0, 1)}
+    opposite_direction = {'U': 'D', 'D': 'U', 'L': 'R', 'R': 'L'}
 
-    # 이동 및 충돌 처리
     for _ in range(2 * n):
-        to_remove = set()  # 충돌로 사라질 구슬의 위치를 기록
-        visited = defaultdict(list)  # 구슬의 위치를 기록
+        cnt = len(beads)
+        visited = set()
+        collision_cnt = 0
 
-        for idx in range(len(beads)):
-            x, y, d = beads[idx]
+        for _ in range(cnt):
+            curr_x, curr_y, d = beads.popleft()
+
             dx, dy = direction_map[d]
-            new_x, new_y = x + dx, y + dy
+            new_x, new_y = curr_x + dx, curr_y + dy
 
-            if 0 <= new_x < n and 0 <= new_y < n:
-                if (new_x, new_y) in visited:
-                    to_remove.add((new_x, new_y))
-                visited[(new_x, new_y)].append((d, (x, y)))
+            if not (0 <= new_x < n and 0 <= new_y < n):
+                # 벽에 부딪히면 방향 반전
+                d = opposite_direction[d]
+                new_x, new_y = curr_x, curr_y
+
+            if (new_x, new_y) not in visited:
+                visited.add((new_x, new_y))
+                beads.append((new_x, new_y, d))
             else:
-                # 벽에 부딪혀 방향 반전
-                if d == 'U': d = 'D'
-                elif d == 'D': d = 'U'
-                elif d == 'L': d = 'R'
-                elif d == 'R': d = 'L'
-                new_x, new_y = x, y  # 이동하지 않음
-                if (new_x, new_y) in visited:
-                    to_remove.add((new_x, new_y))
-                visited[(new_x, new_y)].append((d, (x, y)))
+                collision_cnt += 1
 
-        beads.clear()
-
-        # 충돌 처리 후 남은 구슬을  업데이트
-        for pos, items in visited.items():
-            if pos not in to_remove:
-                for d, original_pos in items:
-                    beads.append((pos[0], pos[1], d))
-
+        check = 0
+        while beads and check < collision_cnt:
+            x, y, d = beads.popleft()
+            if (x, y) in visited:
+                check += 1
+            else:
+                beads.append((x, y, d))
 
     # 남아있는 구슬의 수 계산
     remaining_beads = len(beads)
     return remaining_beads
 
+
+# 입력 처리 및 시뮬레이션
+t = int(input())
+
 for _ in range(t):
     n, m = map(int, input().split())
-    beads = []
+    beads = deque()
     for _ in range(m):
         x, y, d = input().split()
         x, y = int(x) - 1, int(y) - 1
