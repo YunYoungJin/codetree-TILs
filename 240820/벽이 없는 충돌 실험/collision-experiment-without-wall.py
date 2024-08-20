f74ac8c91e4935cdb9ad3e7eb57eaafadb50ee98
Youngjin Yun <30815shine@naver.com>
2024-08-20 22:13:21 +0900

    Title: 벽이 없는 충돌 실험, Time: 6518ms, Memory: 29MB, Status: Passed - CodeTree

--- 240820/벽이 없는 충돌 실험/collision-experiment-without-wall.py
+++ 240820/벽이 없는 충돌 실험/collision-experiment-without-wall.py
@@ -4,8 +4,9 @@ from collections import deque, defaultdict
 
 input = sys.stdin.readline
 
+offset = 2000
 t = int(input())
-direction_map = {'U': (0, 0.5), 'D': (0, -0.5), 'L': (-0.5, 0), 'R': (0.5, 0)}
+direction_map = {'U': (0, 1), 'D': (0, -1), 'L': (-1, 0), 'R': (1, 0)}
 
 for _ in range(t):
     n = int(input())
@@ -14,19 +15,19 @@ for _ in range(t):
 
     for i in range(1, n + 1):
         x, y, w, d = input().split()
-        x, y, w = int(x), int(y), int(w)
+        x, y, w = 2 * int(x) + offset, 2 * int(y) + offset, int(w)
         beads.append((w, i, x, y, d))
 
-    for time in range(1, 4002):
+    for time in range(1, 4001):
         moved_map = defaultdict(list)
-
+        
         for _ in range(len(beads)):
             w, idx, x, y, d = beads.popleft()
 
             dx, dy = direction_map[d]
             new_x, new_y = x + dx, y + dy
 
-            if -2000 <= new_x <= 2000 and -2000 <= new_y <= 2000:
+            if -2000 + offset <= new_x <= 2000 + offset and -2000 + offset <= new_y <= 2000 + offset:
                 heapq.heappush(moved_map[(new_x, new_y)], (-w, -idx, d))
 
         for key in moved_map:
