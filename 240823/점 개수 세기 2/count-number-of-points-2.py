58f5034be6a31a3c6ca931b223eb9fd86a21c865
Youngjin Yun <30815shine@naver.com>
2024-08-23 17:44:08 +0900

    Title: 점 개수 세기 2, Time: 1308ms, Memory: 110MB, Status: Passed - CodeTree

--- 240823/점 개수 세기 2/count-number-of-points-2.py
+++ 240823/점 개수 세기 2/count-number-of-points-2.py
@@ -1,12 +1,11 @@
 import sys
 input = sys.stdin.readline
-from bisect import bisect_left, bisect_right
+from sortedcontainers import SortedSet
 
 n, q = map(int, input().split())
 points = []
-queries = []
-x_coords = set()
-y_coords = set()
+x_coords = SortedSet()
+y_coords = SortedSet()
 
 for _ in range(n):
     x, y = map(int, input().split())
@@ -14,22 +13,11 @@ for _ in range(n):
     x_coords.add(x)
     y_coords.add(y)
 
-for _ in range(q):
-    x1, y1, x2, y2 = map(int, input().split())
-    queries.append((x1, y1, x2, y2))
-    x_coords.add(x1)
-    x_coords.add(x2)
-    y_coords.add(y1)
-    y_coords.add(y2)
+x_map = {x: i for i, x in enumerate(x_coords)}
+y_map = {y: i for i, y in enumerate(y_coords)}
 
-x_sorted = sorted(x_coords)
-y_sorted = sorted(y_coords)
-
-x_map = {x: i for i, x in enumerate(x_sorted)}
-y_map = {y: i for i, y in enumerate(y_sorted)}
-
-max_x = len(x_sorted)
-max_y = len(y_sorted)
+max_x = len(x_coords)
+max_y = len(y_coords)
 
 # Create the 2D grid for prefix sum
 grid = [[0] * (max_y + 1) for _ in range(max_x + 1)]
@@ -48,11 +36,12 @@ for i in range(max_x + 1):
         if i > 0 and j > 0:
             grid[i][j] -= grid[i - 1][j - 1]
 
-for x1, y1, x2, y2 in queries:
-    cx1 = bisect_left(x_sorted, x1)
-    cy1 = bisect_left(y_sorted, y1)
-    cx2 = bisect_right(x_sorted, x2) - 1
-    cy2 = bisect_right(y_sorted, y2) - 1
+for _ in range(q):
+    x1, y1, x2, y2 = map(int, input().split())
+    cx1 = x_coords.bisect_left(x1)
+    cy1 = y_coords.bisect_left(y1)
+    cx2 = x_coords.bisect_right(x2) - 1
+    cy2 = y_coords.bisect_right(y2) - 1
 
     if cx1 > cx2 or cy1 > cy2:
         print(0)
