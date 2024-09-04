7f49207d58844885cf17275195db9333aa926996
Youngjin Yun <30815shine@naver.com>
2024-09-04 21:52:29 +0900

    Title: 간선의 길이를 2배 늘리기, Time: 225ms, Memory: 27MB, Status: Passed - CodeTree

--- 240904/간선의 길이를 2배 늘리기/double-the-length-of-the-edge.py
+++ 240904/간선의 길이를 2배 늘리기/double-the-length-of-the-edge.py
@@ -6,13 +6,12 @@ INF = sys.maxsize
 
 n, m = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
-reverse_graph = [[] for _ in range(n + 1)]
 edges = []
 
 for _ in range(m):
     i, j, d = map(int, input().split())
     graph[i].append((j, d))
-    reverse_graph[j].append((i, d))
+    graph[j].append((i, d))
     edges.append((i, j, d))
 
 
@@ -36,22 +35,25 @@ def dijkstra(start, graph):
     return dist
 
 dist_from_1 = dijkstra(1, graph)
-dist_from_n = dijkstra(n, reverse_graph)
+dist_from_n = dijkstra(n, graph)
 shortest_path = dist_from_1[n]
 longest_path = 0
 
 # 최단 경로가 아닌 간선은 2배를 해도 최단거리에 영향을 미치지 않음
 for a, b, w in edges:
-    if dist_from_1[a] + w + dist_from_n[b] == shortest_path:
+    if dist_from_1[a] + w + dist_from_n[b] == shortest_path or dist_from_1[b] + w + dist_from_n[a] == shortest_path:
         graph[a].remove((b, w))
+        graph[b].remove((a, w))
         graph[a].append((b, 2 * w))
+        graph[b].append((a, 2 * w))
 
         new_dist_from_1 = dijkstra(1, graph)
 
         longest_path = max(longest_path, new_dist_from_1[n])
         
         graph[a].remove((b, 2 * w))
+        graph[b].remove((a, 2 * w))
         graph[a].append((b, w))
-
+        graph[b].append((a, w))
 
 print(abs(longest_path - shortest_path))
