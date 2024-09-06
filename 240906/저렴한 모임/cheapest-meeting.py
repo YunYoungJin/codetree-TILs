20f6c58c1ef180930375ff009bdbdb3df12a5794
Youngjin Yun <30815shine@naver.com>
2024-09-06 16:19:55 +0900

    Title: 저렴한 모임, Time: 190ms, Memory: 28MB, Status: Passed - CodeTree

--- 240906/저렴한 모임/cheapest-meeting.py
+++ 240906/저렴한 모임/cheapest-meeting.py
@@ -23,9 +23,11 @@ for k in range(1, n + 1):
         for j in range(1, n + 1):
             dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
-ans = dist[v1][e] + dist[v2][e]
+ans = INF
 
 for k in range(1, n + 1):
+    if dist[v1][k] == INF or dist[v2][k] == INF or dist[k][e] == INF:
+        continue
     ans = min(ans, dist[v1][k] + dist[v2][k] + dist[k][e])
-    
-print(ans)
+
+print(ans if ans != INF else -1)
