ee60fd8f788180e4e672e36d68a52034bd3380a1
Youngjin Yun <30815shine@naver.com>
2024-09-06 16:17:02 +0900

    Title: 저렴한 모임, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240906/저렴한 모임/cheapest-meeting.py
@@ -0,0 +1,31 @@
+import sys
+INF = sys.maxsize
+
+n, m = map(int, input().split())
+v1, v2, e = map(int, input().split())
+
+dist = [
+    [INF] * (n + 1)
+    for _ in range(n + 1)
+]
+
+for i in range(1, n + 1):
+    dist[i][i] = 0
+
+for i in range(m):
+    x, y, z = map(int, input().split())
+    dist[x][y] = z
+    dist[y][x] = z
+
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+
+ans = dist[v1][e] + dist[v2][e]
+
+for k in range(1, n + 1):
+    ans = min(ans, dist[v1][k] + dist[v2][k] + dist[k][e])
+    
+print(ans)
