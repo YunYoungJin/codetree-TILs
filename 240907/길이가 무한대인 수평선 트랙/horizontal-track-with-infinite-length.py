cec9081014c61959f7c2899503b891f1782a2016
Youngjin Yun <30815shine@naver.com>
2024-09-07 18:50:35 +0900

    Title: 길이가 무한대인 수평선 트랙, Time: 186ms, Memory: 27MB, Status: Passed - CodeTree

--- 240907/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.py
+++ 240907/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.py
@@ -8,11 +8,9 @@ people = [
 ]
 
 arrival_positions = [x + v * t for x, v in people]
-pos_set = set(arrival_positions)
 
 for i in range(len(arrival_positions) - 1, 0, -1):
     if arrival_positions[i - 1] > arrival_positions[i]:
-        pos_set.remove(arrival_positions[i - 1])
         arrival_positions[i - 1] = arrival_positions[i]
 
-print(len(pos_set))
+print(len(set(arrival_positions)))
