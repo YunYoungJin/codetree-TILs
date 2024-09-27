b76b9db4d8c1fa15ba9b6aa5f8acaa3407c3b3de
Youngjin Yun <30815shine@naver.com>
2024-09-27 15:58:10 +0900

    Title: 연결된 정점 2, Time: 353ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240927/연결된 정점 2/connected-vertex-2.py
@@ -0,0 +1,27 @@
+n = int(input())
+uf = list(range(100001))
+size = [1] * (100001)
+
+def union(x, y):
+    X = find(x)
+    Y = find(y)
+
+    if size[X] < size[Y]:
+        uf[X] = Y
+        size[Y] += size[X]
+    else:
+        uf[Y] = X
+        size[X] += size[Y]
+
+    
+def find(x):
+    if uf[x] != x:
+        uf[x] = find(uf[x])
+    
+    return uf[x]
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    union(a, b)
+    
+    print(max(size[find(a)], size[find(b)]))
